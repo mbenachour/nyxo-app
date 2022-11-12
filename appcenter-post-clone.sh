f3aa28c7ffedbb4d5e0d3afadc8311c9c40dd4e0
Mohamed Ben Achour <mohamed.benachour@gmail.com>
2022-11-11 16:33:23 -0800

    Create appcenter-post-clone.sh

---
+++ appcenter-post-clone.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+#!/usr/bin/env bash
+
+# Example: Clone a required repository
+git clone https://github.com/example/SomeProject
+
+# Example: Install App Center CLI
+npm install -g appcenter-cli
+
+#yarn
+
+yarn
+
+react-native link
+
+cd ios && pod install
